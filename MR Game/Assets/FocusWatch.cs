aba0d1b4910f4d93d1eeab2d0e52f582921ec32a
Vlad Baderca <vladfriends@yahoo.ca>
2016-01-10 17:56:14 -0500

    Started working on the focus watch.

---
+++ MR Game/Assets/FocusWatch.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FocusWatch : MonoBehaviour {
+	public Transform secondHand;
+
+	GameObject secondHandClone;
+
+	// Use this for initialization
+	void Start () {
+
+		secondHandClone = (GameObject) Instantiate(secondHand, transform.position, Quaternion.identity);
+		StartCoroutine (CountSeconds());
+	}
+
+	void OnEnable(){
+		print ("Watch was enabled!!");
+	}
+
+	IEnumerator CountSeconds(){
+		yield return null;
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

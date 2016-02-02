0ebd3d078a830c7d13daad40adc66faf055a564d
Vlad Baderca <vladfriends@yahoo.ca>
2016-02-01 21:49:55 -0500

    Started working on level 2

--- MR Game/Assets/NBackAlgo.cs
+++ MR Game/Assets/NBackAlgo.cs
@@ -1,5 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+
+public class NBackCoord {
+	public int x;
+	public int y;
+
+	public NBackCoord()
+	{
+		x = 0;
+		y = 0;
+	}
+
+	public NBackCoord(int x_, int y_){
+		x = x_;
+		y = y_;
+	}
+}
 
 public class NBackAlgo : MonoBehaviour {
 
@@ -14,13 +31,17 @@ public class NBackAlgo : MonoBehaviour {
 
 	Transform squareClone;
 	float timePassed = 0f;
+	List<NBackCoord> NBackCoords = new List<NBackCoord>();
 
 	// Use this for initialization
 	void Start () {
 		squareClone = (Transform) Instantiate(square, transform.position, Quaternion.Euler(new Vector3(0f, 0f,0f)));
 		squareClone.SetParent (this.transform);
+		squareClone.transform.localRotation = Quaternion.Euler (new Vector3 (90f, 180f, 180f));
 
 		squareClone.gameObject.SetActive (false);
+
+		StartCoroutine(ListenForUserInput());
 	}
 	
 	// Update is called once per frame
@@ -42,6 +63,41 @@ public class NBackAlgo : MonoBehaviour {
 	private void PlaceSquare(int x, int y){
 		//moving square in LOCAL COORDINATES
 		squareClone.transform.localPosition = new Vector3 (x*width, 0f, y*height);
+
+		//modifying the n back coordinates
+		NBackCoord newCoord = new NBackCoord (x, y);
+
+		//inserting latest coordinate to the front of the list
+		NBackCoords.Insert (0, newCoord);
+
+		//checking if list is full
+		//if it is, must remove last element of the list
+		if (NBackCoords.Count > NLevel) {
+			NBackCoords.RemoveAt(NLevel);
+		}
+			
+		/*print ("-------------------------------------------");
+		for(int i =0; i< NBackCoords.Count; i++){
+			print (NBackCoords[i].x+", "+NBackCoords[i].y);
+		}*/
+
+	}
+
+	IEnumerator ListenForUserInput() {
+		print ("Started listening for input");
+
+		while (true) {
+			while (NBackCoords.Count > 0) {
+				if (NBackCoords [0].x == NBackCoords [NBackCoords.Count - 1].x
+				   && NBackCoords [0].y == NBackCoords [NBackCoords.Count - 1].y)
+					print ("Same N back level detected");
+
+				yield return new WaitForSeconds(rateOfChange);
+			}
+			yield return new WaitForSeconds(rateOfChange);
+		}
+
+		yield return null;
 	}
 		
 }
